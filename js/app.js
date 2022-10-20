f57faf3b61f6a75ad45e1b29da22b13bf52e0d20
diptanshumahish <diptanshumahish2016@gmail.com>
2022-10-20 08:32:00 +0530

    added navbar resposnisveness

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var menuActive = false;
+document.getElementById("menuButton").addEventListener('click', function () {
+    if (menuActive == false) {
+        document.getElementById("menuPageFull").style.transform = "translateX(0)";
+        menuActive = true;
+        document.getElementById("navigation").style.backgroundColor = " #2d2d47";
+        document.getElementById("navigation").style.color = " #f4f4f4";
+        document.getElementById("menuButton").innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+        document.getElementById("navigation").style.boxShadow = "none";
+        document.getElementById("navigation").style.borderBottom = "0.3px solid #f4f4f480";
+    }
+
+    else {
+        document.getElementById("menuPageFull").style.transform = "translateX(-100vw)";
+        document.getElementById("navigation").style.backgroundColor = "  #f4f4f4";
+        document.getElementById("navigation").style.color = " #111";
+        document.getElementById("menuButton").innerHTML = `<i class="fa-solid fa-bars"></i>`;
+        document.getElementById("navigation").style.boxShadow = " rgba(100, 100, 111, 0.073) 0px 7px 29px 0px";
+        document.getElementById("navigation").style.borderBottom = "unset";
+        menuActive = false;
+    }
+
+})
