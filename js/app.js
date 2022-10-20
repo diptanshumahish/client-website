af53a9dbcd1d7c901ae7eb0e06cd6ca775559b3b
diptanshumahish <diptanshumahish2016@gmail.com>
2022-10-20 08:51:13 +0530

    more stylings on mob nav

--- js/app.js
+++ js/app.js
@@ -21,3 +21,13 @@ document.getElementById("menuButton").addEventListener('click', function () {
     }
 
 })
+
+function closeMenu(){
+    document.getElementById("menuPageFull").style.transform = "translateX(-100vw)";
+    document.getElementById("navigation").style.backgroundColor = "  #f4f4f4";
+    document.getElementById("navigation").style.color = " #111";
+    document.getElementById("menuButton").innerHTML = `<i class="fa-solid fa-bars"></i>`;
+    document.getElementById("navigation").style.boxShadow = " rgba(100, 100, 111, 0.073) 0px 7px 29px 0px";
+    document.getElementById("navigation").style.borderBottom = "unset";
+    menuActive = false;
+}
